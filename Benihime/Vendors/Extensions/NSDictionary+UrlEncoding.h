e04eb30066a36fa1c0463a243c02a6e78aa2505d
Shiki <shikishiji@gmail.com>
2012-01-30 11:28:23 +0800

    Added vendor category NSDictionary+UrlEncoding

---
+++ Benihime/Vendors/Extensions/NSDictionary+UrlEncoding.h
@@ -0,0 +1,16 @@
+//
+//  NSDictionary+UrlEncoding.h
+//  
+//
+//  Source: http://blog.ablepear.com/2008/12/urlencoding-category-for-nsdictionary.html
+//  
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSDictionary (UrlEncoding)
+
+-(NSString*) urlEncodedString;  
+
+@end
