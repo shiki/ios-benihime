9f20bbc22f41877b62223fe66b7c2261526450f6
Shiki <shikishiji@gmail.com>
2012-02-07 18:57:33 +0800

    Added BCGRectSetY() function on BCGeometry

--- Benihime/BCGeometry.h
+++ Benihime/BCGeometry.h
@@ -8,6 +8,7 @@
 #import <Foundation/Foundation.h>
 
 CGRect BCGRectSetHeight(CGRect rect, CGFloat height);
+CGRect BCGRectSetY(CGRect rect, CGFloat y);
 
 CGRect BCGRectAdd(CGRect rect, CGFloat x, CGFloat y, CGFloat width, CGFloat height);
 
