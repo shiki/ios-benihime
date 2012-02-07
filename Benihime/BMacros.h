3962ae05abc8a5bf487d70c0b0a87ac0ddc79ef1
Shiki <shikishiji@gmail.com>
2012-02-07 18:56:48 +0800

    Added additional macros for checking if device is iPad and checking the current orientation.

--- Benihime/BMacros.h
+++ Benihime/BMacros.h
@@ -6,3 +6,8 @@
 
 // stolen from Three20
 #define B_RELEASE_SAFELY(__POINTER) { [__POINTER release]; __POINTER = nil; }
+
+#define B_UI_IS_IPAD() UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad
+
+#define B_UI_ORIENTATION_IS_PORTRAIT() UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].statusBarOrientation)
+#define B_UI_ORIENTATION_IS_LANDSCAPE() UIInterfaceOrientationIsLandscape([UIApplication sharedApplication].statusBarOrientation)
