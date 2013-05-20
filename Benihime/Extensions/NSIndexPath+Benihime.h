28477230f5f84dc379d1b2781f2a8b70ab2a6743
Shiki <shikishiji@gmail.com>
2013-05-20 13:52:37 +0800

    NSIndexPath category: Added helper for creating sequential NSIndexPath instances.

---
+++ Benihime/Extensions/NSIndexPath+Benihime.h
@@ -0,0 +1,22 @@
+//
+//  NSIndexPath+Benihime.h
+//  Benihime
+//
+//  Created by Shiki on 5/20/13.
+//  Copyright (c) 2013 FuFu Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+@interface NSIndexPath (Benihime)
+
+// Creates an array of NSIndexPath instances with row numbers from the given `startingRow` counting
+// up to a total of `count`. For example, if `startingRow` is 3 and the `count` is 4, this will create
+// index paths with these rows: 3, 4, 5, 6.
++ (NSArray *)sequentialIndexPathsFromRow:(NSUInteger)startingRow withCount:(NSUInteger)count
+                               inSection:(NSUInteger)section;
+
+@end
---
+++ Benihime/Extensions/NSIndexPath+Benihime.m
@@ -0,0 +1,24 @@
+//
+//  NSIndexPath+Benihime.m
+//  Benihime
+//
+//  Created by Shiki on 5/20/13.
+//  Copyright (c) 2013 FuFu Labs. All rights reserved.
+//
+
+#import "NSIndexPath+Benihime.h"
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+@implementation NSIndexPath (Benihime)
+
++ (NSArray *)sequentialIndexPathsFromRow:(NSUInteger)startingRow withCount:(NSUInteger)count
+                               inSection:(NSUInteger)section {
+  NSMutableArray *ret = [NSMutableArray arrayWithCapacity:count];
+  for (int i = 0; i < count; i++) {
+    [ret addObject:[NSIndexPath indexPathForRow:startingRow + i inSection:section]];
+  }
+  return [ret copy];
+}
+
+@end
